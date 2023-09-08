a48cfce37e17ba8001d9cf88c4ea51420c1540f1
DevSauloCouto <saulocouto1221@gmail.com>
2023-09-08 00:27:19 -0300

    alterei código javascript

--- script/scriptCalculadora.js
+++ script/scriptCalculadora.js
@@ -1,52 +1,22 @@
 let paragrafo = document.getElementById("entrada");
-let buttons = {
-    ponto: document.getElementsByClassName("ponto").innerHTML = '.',
-    zero: document.getElementsByClassName("zero").innerHTML = '0',
-    um: document.getElementsByClassName("um").innerHTML = '1',
-    dois: document.getElementsByClassName("dois").innerHTML = '2',
-    tres: document.getElementsByClassName("tres").innerHTML = '3',
-    quatro: document.getElementsByClassName("quatro").innerHTML = '4',
-    cinco: document.getElementsByClassName("cinco").innerHTML = '5',
-    seis: document.getElementsByClassName("seis").innerHTML = '6', 
-    sete: document.getElementsByClassName("sete").innerHTML = '7',
-    oito: document.getElementsByClassName("oito").innerHTML = '8',
-    nove: document.getElementsByClassName("nove").innerHTML = '9'
-};
-let operators = {
-    soma: document.getElementsByClassName("soma").innerHTML = ' + ',
-    subtracao: document.getElementsByClassName("subtracao").innerHTML = ' - ',
-    multiplicacao: document.getElementsByClassName("mult").innerHTML = ' * ',
-    divisao: document.getElementsByClassName("divisao").innerHTML = ' / ',
-    porcentagem: document.getElementsByClassName("percent").innerHTML = ' % '
-}
+const numeros = ["0","1","2","3","4","5","6","7","8","9","."];
+const operadores = [" + "," - "," * "," / "," % "];
 
 //função que insere e trata os números selecionados pelo usuário..
-function number(prop){
-    paragrafo.innerHTML += buttons[prop];
-    value1 = paragrafo.innerHTML;
-    console.log(value1);
+function insertNumber(num){
+    paragrafo.innerHTML += numeros[num]; 
+    value = paragrafo.innerHTML;
+}
 
-    numeros = {
-        num1: parseInt(value1),
-        num2: ''
-    };
+//função que insere um sinal de operação selecionado pelo usuário..
+function insertOperador(n){
+    paragrafo.innerHTML += operadores[n];
+    value = paragrafo.innerHTML;
+}
 
-    if(value1.length == 5){
-        numeros.num2 = parseInt(value1.substring(4));
-    } else if (value1.length == 6){
-        numeros.num2 = parseInt(value1.substring(4)) || parseInt(value1.substring(5));
-    } else if (value1.length == 7){
-        numeros.num2 = parseInt(value1.substring(4)) || parseInt(value1.substring(5));
-    } else if (value1.length == 8){
-        numeros.num2 = parseInt(value1.substring(4)) || parseInt(value1.substring(5)) || parseInt(value1.substring(6));
-    } else if (value1.length == 9){
-        numeros.num2 = parseInt(value1.substring(4)) || parseInt(value1.substring(5)) || parseInt(value1.substring(6)) || parseInt(value1.substring(7));
-    } else if (value1.length == 10){
-        numeros.num2 = parseInt(value1.substring(4)) || parseInt(value1.substring(5)) || parseInt(value1.substring(6)) || parseInt(value1.substring(7)) || parseInt(value1.substring(8));
-    } else if(value1.length == 11){
-        numeros.num2 = parseInt(value1.substring(4)) || parseInt(value1.substring(5)) || parseInt(value1.substring(6)) || parseInt(value1.substring(7)) || parseInt(value1.substring(8)) || parseInt(value1.substring(9));
-    }
-    console.log(numeros);
+//função que apresenta o resultado da operação..
+function resultado(){
+    return paragrafo.innerHTML = eval(value);
 }
 
 //função que apaga os números inseridos pelo usuário..
@@ -60,30 +30,9 @@ function back(){
     paragrafo.innerHTML = backspace.substring(0, backspace.length - 1);
 }
 
-//função que insere um sinal de operação selecionado pelo usuário..
-function operation(prop){
-    paragrafo.innerHTML += operators[prop];
-    operador = {
-        sinal: operators[prop]
-    };
-    console.log(operador);
-    return true;
-}
 
-//função que apresenta o resultado da operação..
-function resultado(){
-    if(operador.sinal == " + "){
-        paragrafo.innerHTML = numeros.num1 + numeros.num2;
-    } else if(operador.sinal == " - "){
-        paragrafo.innerHTML = numeros.num1 - numeros.num2;
-    } else if(operador.sinal == " * "){
-        paragrafo.innerHTML = numeros.num1 * numeros.num2;
-    } else if(operador.sinal == " / "){
-        paragrafo.innerHTML = numeros.num1 / numeros.num2;
-    } else if(operador.sinal == " % "){
-        paragrafo.innerHTML = (numeros.num1 / 100) * numeros.num2;
-    }
-}
+
+
 
 
 
